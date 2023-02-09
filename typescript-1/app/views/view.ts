a9e2292673a0657a98fb498e8c12a90fa6a661c1
Rafael Ederli <rafaederli@gmail.com>
2023-02-09 16:30:50 -0300

    update - continuation of the course

---
+++ typescript-1/app/views/view.ts
@@ -0,0 +1,27 @@
+export abstract class View<T> {
+
+    protected element: HTMLElement;
+    private escapar = false;
+
+    constructor(seletor: string, escapar?: boolean) {
+        const elemento = document.querySelector(seletor);
+        if (elemento) {
+            this.element = elemento as HTMLElement;
+        } else {
+            throw Error(`Seletor ${seletor} não existe no DOM. Verifique`);
+        }
+        if (escapar) {
+            this.escapar = escapar;
+        }
+    }
+
+    protected abstract template(model: T): string;
+
+    public update(model: T): void {
+        let template = this.template(model);
+        if (this.escapar) {
+            template = template.replace(/<script>[\s\S]*?<\/script>/, '');
+        }
+        this.element.innerHTML = template;
+    }
+}
